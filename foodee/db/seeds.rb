a01ee1cd8ccd95ae49de5d67eb8400c0d9076769
Jasmine Chabra <jchabra@gmail.com>
2012-11-16 14:50:22 -0500

    updated seeds

--- foodee/db/seeds.rb
+++ foodee/db/seeds.rb
@@ -1,11 +1,16 @@
+Restaurant.delete_all
+Dish.delete_all
+
 Restaurant.create(:name => "McDonalds", :cuisine => "Fast Food, American", :address => "New York")
 Restaurant.create(:name => "China House", :cuisine => "Chinese", :address => "New York")
 Restaurant.create(:name => "Shake Shack", :cuisine => "Burgers", :address => "New York")
 Restaurant.create(:name => "Chipotle", :cuisine => "Fast Food, Mexican", :address => "New York")
 
-Dish.create(:name => "McNuggets", :cost => "$3.98", :photo => "http://imgs.sfgate.com/blogs/images/sfgate/sfmoms/2010/06/28/mcnugget.jpg")
-Dish.create(:name => "Chicken Lo Mein", :cost => "$6.50", :photo => "http://talkingpretty.com/wp-content/uploads/2012/08/chicken-lomein-recipe-photo.jpg")
-Dish.create(:name => "ShackBurger", :cost => "$3.98", :photo => "http://blogs.miaminewtimes.com/shortorder/shack%20burger.jpg")
-Dish.create(:name => "Burrito", :cost => "$4.55", :photo => "http://blogs.citypages.com/food/Chipotle%20500%20kjhkjhkjh.jpg")
+
+Dish.create(:name => "McNuggets", :cost => "$13.98", :photo => 'http://imgs.sfgate.com/blogs/images/sfgate/sfmoms/2010/06/28/mcnugget.jpg')
+Dish.create(:name => "Chicken Lo Mein", :cost => "$16.50", :photo => 'http://talkingpretty.com/wp-content/uploads/2012/08/chicken-lomein-recipe-photo.jpg')
+Dish.create(:name => "ShackBurger", :cost => "$13.98", :photo => 'http://blogs.miaminewtimes.com/shortorder/shack%20burger.jpg')
+Dish.create(:name => "Burrito", :cost => "$14.55", :photo => 'http://blogs.citypages.com/food/Chipotle%20500%20kjhkjhkjh.jpg')
+
 
 
