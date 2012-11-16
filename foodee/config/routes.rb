efbc2402bc8f00b3d41eb2932a6b3c73addf3c59
Jasmine Chabra <jchabra@gmail.com>
2012-11-16 11:45:16 -0500

    jasmine changes

--- foodee/config/routes.rb
+++ foodee/config/routes.rb
@@ -1,4 +1,9 @@
 Foodee::Application.routes.draw do
+
+
+
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ Foodee::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
