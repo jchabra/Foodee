524f76264111d65b8165467f1f33199771c69418
Adrian <adrianbautista8@gmail.com>
2012-11-16 13:30:56 -0500

    dishes functional

--- foodee/app/controllers/dishes_controller.rb
+++ foodee/app/controllers/dishes_controller.rb
@@ -4,25 +4,37 @@ class DishesController < ApplicationController
     # creates an array of all the dishes in a restaurant
   end
   def show
+    dish_id = params[:id]
     @dish = Dish.find(dish_id)
+    binding.pry
   end
   def new
     @dish = Dish.new
   end
   def edit
+    @dish = Dish.find(params[:id])
   end
   def create
     @dish = Dish.new(params[:dish])
-
     if @dish.save
       redirect_to dishes_path
       # redirect to dishes page if saved properly
-    end
+    else
       render :new
       # stay on the form page
+    end
   end
   def update
+    @dish = Dish.find(params[:id])
+    if @dish.update_attributes(params[:dish])
+      redirect_to @dish
+    else
+      render :edit
+    end
   end
   def destroy
+    dish = Dish.find(params[:id])
+    dish.delete
+    redirect_to dishes_path
   end
 end
