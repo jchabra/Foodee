94f57009cbd06bf9333dc9415830c6494c704d8c
Adrian <adrianbautista8@gmail.com>
2012-11-16 12:12:24 -0500

    dish stuff

--- foodee/app/controllers/dish_controller.rb
+++ foodee/app/controllers/dish_controller.rb
@@ -1,14 +1,24 @@
 class DishController < ApplicationController
   def index
+    @dishes = Dish.all
+    # creates an array of all the dishes in a restaurant
   end
   def show
+    @dish = Dish.find(dish_id)
   end
   def new
   end
   def edit
   end
   def create
-    @dish = Dish.new
+    @dish = Dish.new(params[:dish])
+
+    if @dish.save
+      redirect_to dishes_path
+      # redirect to dishes page if saved properly
+    end
+      render :new
+      # stay on the form page
   end
   def update
   end
