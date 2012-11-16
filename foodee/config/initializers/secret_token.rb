aa7114ac55be998118202fd822ca0de8ef931085
Adrian <adrianbautista8@gmail.com>
2012-11-16 10:52:24 -0500

    intial files for foodee

---
+++ foodee/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodee::Application.config.secret_token = 'f26f8445b64aaa938d6d8710d8015590f72dd969dfbb7658b6548ccfa19ff5327cb455fc753bda6d52d155e84593cf9de9acbcd340c51bf3a7dcdcbe9aef229c'
